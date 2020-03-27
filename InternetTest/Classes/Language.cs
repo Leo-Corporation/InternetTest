0878bd9f84cde491f0de425dc174a3d8eb6c52e9
Léo Peyronnet <leo.peyronnet@outlook.fr>
2020-03-27 12:02:08 +0100

    Ajout de fonctions au niveau des langues

--- InternetTest/Classes/Language.cs
+++ InternetTest/Classes/Language.cs
@@ -6,7 +6,38 @@ using System.Threading.Tasks;
 
 namespace InternetTest.Classes
 {
-    class Language
+    public class Language
     {
+        public string GetName(string returnLang)
+        {
+            string res = "";
+            if (returnLang == "fr") // Si la langue est française
+            {
+                if (Properties.Settings.Default.Language == "fr-FR")
+                {
+                    res = "Français (fr-FR)";
+                }
+                else if (Properties.Settings.Default.Language == "EN")
+                {
+                    res = "Anglais (EN)";
+                }
+            }
+            else if (returnLang == "en") // Si la langue est anglaise
+            {
+                if (Properties.Settings.Default.Language == "fr-FR")
+                {
+                    res = "French (fr-FR)";
+                }
+                else if (Properties.Settings.Default.Language == "EN")
+                {
+                    res = "English (EN)";
+                }
+            }
+            return res;
+        }
+        public string GetCode()
+        {
+            return Properties.Settings.Default.Language;
+        }
     }
 }
