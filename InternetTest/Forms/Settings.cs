b9b2d64f665e6e70ee51f437ef226f207e8aff7c
Léo Peyronnet <leo.peyronnet@outlook.fr>
2020-05-07 11:34:14 +0200

    Le message qui s'affiche lorsque les données sont effacées n'est pas dans la bonne langue (#90) + Mise à jour de la version

--- InternetTest/Forms/Settings.cs
+++ InternetTest/Forms/Settings.cs
@@ -205,14 +205,14 @@ namespace InternetTest.Forms
         {
             Properties.Settings.Default.Reset();
             Properties.Settings.Default.Save();
-            if (new Language().GetCode() == "fr-FR")
+            if (gunaComboBox1.Text.Contains("fr-FR"))
             {
                 gunaComboBox1.Text = "Français (fr-FR)";
                 gunaWinSwitch1.Checked = false;
                 gunaWinSwitch2.Checked = true;
                 MessageBox.Show("Les données ont été effacées.", "Terminé", MessageBoxButtons.OK, MessageBoxIcon.Information);
             }
-            else if (new Language().GetCode() == "EN")
+            else if (gunaComboBox1.Text.Contains("EN"))
             {
                 gunaComboBox1.Text = "French (fr-FR)";
                 gunaWinSwitch1.Checked = false;
