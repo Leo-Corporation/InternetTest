e5ca6c461baf6fdb0c44fa1c213cf2f6247c5f6d
Léo Peyronnet <leo.peyronnet@outlook.fr>
2020-07-04 10:38:11 +0200

    Ajouter l'option d'effectuer un test au démarrage (#104)

--- InternetTest/Properties/Settings.Designer.cs
+++ InternetTest/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace InternetTest.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.5.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.6.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -68,6 +68,18 @@ namespace InternetTest.Properties {
             }
             set {
                 this["TestSite"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("True")]
+        public bool TestOnStart {
+            get {
+                return ((bool)(this["TestOnStart"]));
+            }
+            set {
+                this["TestOnStart"] = value;
             }
         }
     }
