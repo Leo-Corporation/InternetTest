49d4ac7ac0c4fa78379292d47a38499a453d9e3f
Léo Peyronnet <leo.peyronnet@outlook.fr>
2020-08-01 11:55:34 +0200

    Ajout d'un nouveau système de version

---
+++ InternetTest/Classes/Definitions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InternetTest.Classes
+{
+    public static class Definitions
+    {
+        public static readonly string Version = "4.5.0.2008-rc1"; // Version
+    }
+}
