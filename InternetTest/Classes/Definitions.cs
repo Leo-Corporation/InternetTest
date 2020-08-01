634e5f00b37b9d2f9499235f7f81eb81c56151ac
Léo Peyronnet <leo.peyronnet@outlook.fr>
2020-08-01 13:14:25 +0200

    Update Definitions.cs

--- InternetTest/Classes/Definitions.cs
+++ InternetTest/Classes/Definitions.cs
@@ -8,6 +8,6 @@ namespace InternetTest.Classes
 {
     public static class Definitions
     {
-        public static readonly string Version = "4.5.0.2008-rc1"; // Version
+        public static readonly string Version = "4.5.0.2008-rc2"; // Version
     }
 }
