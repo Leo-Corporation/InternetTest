7453c33d17bde8e73ad98b2377c8b939874d161d
Léo Peyronnet <leo.peyronnet@outlook.fr>
2020-12-03 14:19:16 +0100

    Added the "Test" page (#156)

---
+++ InternetTest/UserControls/SettingsPages/Test.Designer.cs
@@ -0,0 +1,327 @@
+﻿
+namespace InternetTest.UserControls.SettingsPages
+{
+    partial class Test
+    {
+        /// <summary> 
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur de composants
+
+        /// <summary> 
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas 
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gunaComboBox2 = new Guna.UI.WinForms.GunaComboBox();
+            this.gunaWinSwitch3 = new Guna.UI.WinForms.GunaWinSwitch();
+            this.gunaLabel17 = new Guna.UI.WinForms.GunaLabel();
+            this.gunaLinkLabel2 = new Guna.UI.WinForms.GunaLinkLabel();
+            this.gunaLabel14 = new Guna.UI.WinForms.GunaLabel();
+            this.gunaLabel19 = new Guna.UI.WinForms.GunaLabel();
+            this.gunaLabel16 = new Guna.UI.WinForms.GunaLabel();
+            this.gunaLabel13 = new Guna.UI.WinForms.GunaLabel();
+            this.gunaWinSwitch2 = new Guna.UI.WinForms.GunaWinSwitch();
+            this.gunaLabel7 = new Guna.UI.WinForms.GunaLabel();
+            this.gunaLabel6 = new Guna.UI.WinForms.GunaLabel();
+            this.gunaLabel18 = new Guna.UI.WinForms.GunaLabel();
+            this.gunaLabel15 = new Guna.UI.WinForms.GunaLabel();
+            this.gunaLabel12 = new Guna.UI.WinForms.GunaLabel();
+            this.gunaLabel5 = new Guna.UI.WinForms.GunaLabel();
+            this.gunaGradientButton1 = new Guna.UI.WinForms.GunaGradientButton();
+            this.SuspendLayout();
+            // 
+            // gunaComboBox2
+            // 
+            this.gunaComboBox2.BackColor = System.Drawing.Color.Transparent;
+            this.gunaComboBox2.BaseColor = System.Drawing.Color.White;
+            this.gunaComboBox2.BorderColor = System.Drawing.Color.Silver;
+            this.gunaComboBox2.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed;
+            this.gunaComboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.gunaComboBox2.FocusedColor = System.Drawing.Color.Empty;
+            this.gunaComboBox2.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold);
+            this.gunaComboBox2.ForeColor = System.Drawing.Color.Black;
+            this.gunaComboBox2.FormattingEnabled = true;
+            this.gunaComboBox2.Items.AddRange(new object[] {
+            "Bing Maps",
+            "Google Maps"});
+            this.gunaComboBox2.Location = new System.Drawing.Point(15, 249);
+            this.gunaComboBox2.Name = "gunaComboBox2";
+            this.gunaComboBox2.OnHoverItemBaseColor = System.Drawing.Color.RoyalBlue;
+            this.gunaComboBox2.OnHoverItemForeColor = System.Drawing.Color.White;
+            this.gunaComboBox2.Radius = 6;
+            this.gunaComboBox2.Size = new System.Drawing.Size(181, 28);
+            this.gunaComboBox2.TabIndex = 26;
+            // 
+            // gunaWinSwitch3
+            // 
+            this.gunaWinSwitch3.BackColor = System.Drawing.Color.Transparent;
+            this.gunaWinSwitch3.BaseColor = System.Drawing.SystemColors.Control;
+            this.gunaWinSwitch3.Checked = true;
+            this.gunaWinSwitch3.CheckedOffColor = System.Drawing.Color.DarkGray;
+            this.gunaWinSwitch3.CheckedOnColor = System.Drawing.Color.RoyalBlue;
+            this.gunaWinSwitch3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.gunaWinSwitch3.FillColor = System.Drawing.Color.White;
+            this.gunaWinSwitch3.Location = new System.Drawing.Point(251, 173);
+            this.gunaWinSwitch3.Name = "gunaWinSwitch3";
+            this.gunaWinSwitch3.Size = new System.Drawing.Size(40, 22);
+            this.gunaWinSwitch3.TabIndex = 25;
+            this.gunaWinSwitch3.CheckedChanged += new System.EventHandler(this.gunaWinSwitch3_CheckedChanged);
+            // 
+            // gunaLabel17
+            // 
+            this.gunaLabel17.AutoSize = true;
+            this.gunaLabel17.BackColor = System.Drawing.Color.Transparent;
+            this.gunaLabel17.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold);
+            this.gunaLabel17.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.gunaLabel17.Location = new System.Drawing.Point(297, 175);
+            this.gunaLabel17.Name = "gunaLabel17";
+            this.gunaLabel17.Size = new System.Drawing.Size(45, 17);
+            this.gunaLabel17.TabIndex = 24;
+            this.gunaLabel17.Text = "Activé";
+            // 
+            // gunaLinkLabel2
+            // 
+            this.gunaLinkLabel2.AutoSize = true;
+            this.gunaLinkLabel2.BackColor = System.Drawing.Color.Transparent;
+            this.gunaLinkLabel2.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold);
+            this.gunaLinkLabel2.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.gunaLinkLabel2.LinkColor = System.Drawing.Color.RoyalBlue;
+            this.gunaLinkLabel2.Location = new System.Drawing.Point(59, 116);
+            this.gunaLinkLabel2.Name = "gunaLinkLabel2";
+            this.gunaLinkLabel2.Size = new System.Drawing.Size(61, 17);
+            this.gunaLinkLabel2.TabIndex = 23;
+            this.gunaLinkLabel2.TabStop = true;
+            this.gunaLinkLabel2.Text = "Modifier";
+            this.gunaLinkLabel2.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.gunaLinkLabel2_LinkClicked);
+            // 
+            // gunaLabel14
+            // 
+            this.gunaLabel14.AutoSize = true;
+            this.gunaLabel14.BackColor = System.Drawing.Color.Transparent;
+            this.gunaLabel14.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold);
+            this.gunaLabel14.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.gunaLabel14.Location = new System.Drawing.Point(14, 115);
+            this.gunaLabel14.Name = "gunaLabel14";
+            this.gunaLabel14.Size = new System.Drawing.Size(39, 17);
+            this.gunaLabel14.TabIndex = 19;
+            this.gunaLabel14.Text = "{URL}";
+            // 
+            // gunaLabel19
+            // 
+            this.gunaLabel19.AutoSize = true;
+            this.gunaLabel19.BackColor = System.Drawing.Color.Transparent;
+            this.gunaLabel19.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold);
+            this.gunaLabel19.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.gunaLabel19.Location = new System.Drawing.Point(13, 229);
+            this.gunaLabel19.Name = "gunaLabel19";
+            this.gunaLabel19.Size = new System.Drawing.Size(225, 17);
+            this.gunaLabel19.TabIndex = 20;
+            this.gunaLabel19.Text = "Ouvrir la localisation d\'une IP dans :";
+            // 
+            // gunaLabel16
+            // 
+            this.gunaLabel16.AutoSize = true;
+            this.gunaLabel16.BackColor = System.Drawing.Color.Transparent;
+            this.gunaLabel16.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold);
+            this.gunaLabel16.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.gunaLabel16.Location = new System.Drawing.Point(13, 173);
+            this.gunaLabel16.Name = "gunaLabel16";
+            this.gunaLabel16.Size = new System.Drawing.Size(183, 17);
+            this.gunaLabel16.TabIndex = 21;
+            this.gunaLabel16.Text = "Lancer un test au démarrage";
+            // 
+            // gunaLabel13
+            // 
+            this.gunaLabel13.AutoSize = true;
+            this.gunaLabel13.BackColor = System.Drawing.Color.Transparent;
+            this.gunaLabel13.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold);
+            this.gunaLabel13.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.gunaLabel13.Location = new System.Drawing.Point(14, 98);
+            this.gunaLabel13.Name = "gunaLabel13";
+            this.gunaLabel13.Size = new System.Drawing.Size(216, 17);
+            this.gunaLabel13.TabIndex = 22;
+            this.gunaLabel13.Text = "Tester votre connexion sur le site :";
+            // 
+            // gunaWinSwitch2
+            // 
+            this.gunaWinSwitch2.BackColor = System.Drawing.Color.Transparent;
+            this.gunaWinSwitch2.BaseColor = System.Drawing.SystemColors.Control;
+            this.gunaWinSwitch2.Checked = true;
+            this.gunaWinSwitch2.CheckedOffColor = System.Drawing.Color.DarkGray;
+            this.gunaWinSwitch2.CheckedOnColor = System.Drawing.Color.RoyalBlue;
+            this.gunaWinSwitch2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.gunaWinSwitch2.FillColor = System.Drawing.Color.White;
+            this.gunaWinSwitch2.Location = new System.Drawing.Point(251, 39);
+            this.gunaWinSwitch2.Name = "gunaWinSwitch2";
+            this.gunaWinSwitch2.Size = new System.Drawing.Size(40, 22);
+            this.gunaWinSwitch2.TabIndex = 18;
+            this.gunaWinSwitch2.CheckedChanged += new System.EventHandler(this.gunaWinSwitch2_CheckedChanged);
+            // 
+            // gunaLabel7
+            // 
+            this.gunaLabel7.AutoSize = true;
+            this.gunaLabel7.BackColor = System.Drawing.Color.Transparent;
+            this.gunaLabel7.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold);
+            this.gunaLabel7.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.gunaLabel7.Location = new System.Drawing.Point(297, 41);
+            this.gunaLabel7.Name = "gunaLabel7";
+            this.gunaLabel7.Size = new System.Drawing.Size(45, 17);
+            this.gunaLabel7.TabIndex = 12;
+            this.gunaLabel7.Text = "Activé";
+            // 
+            // gunaLabel6
+            // 
+            this.gunaLabel6.AutoSize = true;
+            this.gunaLabel6.BackColor = System.Drawing.Color.Transparent;
+            this.gunaLabel6.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold);
+            this.gunaLabel6.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.gunaLabel6.Location = new System.Drawing.Point(14, 41);
+            this.gunaLabel6.Name = "gunaLabel6";
+            this.gunaLabel6.Size = new System.Drawing.Size(155, 17);
+            this.gunaLabel6.TabIndex = 13;
+            this.gunaLabel6.Text = "Notifier des mises à jour";
+            // 
+            // gunaLabel18
+            // 
+            this.gunaLabel18.AutoSize = true;
+            this.gunaLabel18.BackColor = System.Drawing.Color.Transparent;
+            this.gunaLabel18.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Bold);
+            this.gunaLabel18.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.gunaLabel18.Location = new System.Drawing.Point(11, 201);
+            this.gunaLabel18.Name = "gunaLabel18";
+            this.gunaLabel18.Size = new System.Drawing.Size(72, 28);
+            this.gunaLabel18.TabIndex = 14;
+            this.gunaLabel18.Text = "Cartes";
+            // 
+            // gunaLabel15
+            // 
+            this.gunaLabel15.AutoSize = true;
+            this.gunaLabel15.BackColor = System.Drawing.Color.Transparent;
+            this.gunaLabel15.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Bold);
+            this.gunaLabel15.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.gunaLabel15.Location = new System.Drawing.Point(11, 145);
+            this.gunaLabel15.Name = "gunaLabel15";
+            this.gunaLabel15.Size = new System.Drawing.Size(117, 28);
+            this.gunaLabel15.TabIndex = 15;
+            this.gunaLabel15.Text = "Démarrage";
+            // 
+            // gunaLabel12
+            // 
+            this.gunaLabel12.AutoSize = true;
+            this.gunaLabel12.BackColor = System.Drawing.Color.Transparent;
+            this.gunaLabel12.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Bold);
+            this.gunaLabel12.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.gunaLabel12.Location = new System.Drawing.Point(12, 70);
+            this.gunaLabel12.Name = "gunaLabel12";
+            this.gunaLabel12.Size = new System.Drawing.Size(119, 28);
+            this.gunaLabel12.TabIndex = 16;
+            this.gunaLabel12.Text = "Site de test";
+            // 
+            // gunaLabel5
+            // 
+            this.gunaLabel5.AutoSize = true;
+            this.gunaLabel5.BackColor = System.Drawing.Color.Transparent;
+            this.gunaLabel5.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Bold);
+            this.gunaLabel5.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.gunaLabel5.Location = new System.Drawing.Point(12, 13);
+            this.gunaLabel5.Name = "gunaLabel5";
+            this.gunaLabel5.Size = new System.Drawing.Size(136, 28);
+            this.gunaLabel5.TabIndex = 17;
+            this.gunaLabel5.Text = "Notifications";
+            // 
+            // gunaGradientButton1
+            // 
+            this.gunaGradientButton1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.gunaGradientButton1.Animated = true;
+            this.gunaGradientButton1.AnimationHoverSpeed = 0.07F;
+            this.gunaGradientButton1.AnimationSpeed = 0.03F;
+            this.gunaGradientButton1.BackColor = System.Drawing.Color.Transparent;
+            this.gunaGradientButton1.BaseColor1 = System.Drawing.Color.DeepSkyBlue;
+            this.gunaGradientButton1.BaseColor2 = System.Drawing.Color.RoyalBlue;
+            this.gunaGradientButton1.BorderColor = System.Drawing.Color.Black;
+            this.gunaGradientButton1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.gunaGradientButton1.DialogResult = System.Windows.Forms.DialogResult.None;
+            this.gunaGradientButton1.FocusedColor = System.Drawing.Color.Empty;
+            this.gunaGradientButton1.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Bold);
+            this.gunaGradientButton1.ForeColor = System.Drawing.Color.White;
+            this.gunaGradientButton1.Image = global::InternetTest.Properties.Resources.checkmark;
+            this.gunaGradientButton1.ImageSize = new System.Drawing.Size(20, 20);
+            this.gunaGradientButton1.Location = new System.Drawing.Point(221, 362);
+            this.gunaGradientButton1.Name = "gunaGradientButton1";
+            this.gunaGradientButton1.OnHoverBaseColor1 = System.Drawing.Color.DodgerBlue;
+            this.gunaGradientButton1.OnHoverBaseColor2 = System.Drawing.Color.MediumBlue;
+            this.gunaGradientButton1.OnHoverBorderColor = System.Drawing.Color.Black;
+            this.gunaGradientButton1.OnHoverForeColor = System.Drawing.Color.White;
+            this.gunaGradientButton1.OnHoverImage = null;
+            this.gunaGradientButton1.OnPressedColor = System.Drawing.Color.Black;
+            this.gunaGradientButton1.Radius = 6;
+            this.gunaGradientButton1.Size = new System.Drawing.Size(127, 33);
+            this.gunaGradientButton1.TabIndex = 27;
+            this.gunaGradientButton1.Text = "Appliquer";
+            this.gunaGradientButton1.Click += new System.EventHandler(this.gunaGradientButton1_Click);
+            // 
+            // Test
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.Controls.Add(this.gunaGradientButton1);
+            this.Controls.Add(this.gunaComboBox2);
+            this.Controls.Add(this.gunaWinSwitch3);
+            this.Controls.Add(this.gunaLabel17);
+            this.Controls.Add(this.gunaLinkLabel2);
+            this.Controls.Add(this.gunaLabel14);
+            this.Controls.Add(this.gunaLabel19);
+            this.Controls.Add(this.gunaLabel16);
+            this.Controls.Add(this.gunaLabel13);
+            this.Controls.Add(this.gunaWinSwitch2);
+            this.Controls.Add(this.gunaLabel7);
+            this.Controls.Add(this.gunaLabel6);
+            this.Controls.Add(this.gunaLabel18);
+            this.Controls.Add(this.gunaLabel15);
+            this.Controls.Add(this.gunaLabel12);
+            this.Controls.Add(this.gunaLabel5);
+            this.Name = "Test";
+            this.Size = new System.Drawing.Size(568, 411);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Guna.UI.WinForms.GunaComboBox gunaComboBox2;
+        private Guna.UI.WinForms.GunaWinSwitch gunaWinSwitch3;
+        private Guna.UI.WinForms.GunaLabel gunaLabel17;
+        private Guna.UI.WinForms.GunaLinkLabel gunaLinkLabel2;
+        private Guna.UI.WinForms.GunaLabel gunaLabel14;
+        private Guna.UI.WinForms.GunaLabel gunaLabel19;
+        private Guna.UI.WinForms.GunaLabel gunaLabel16;
+        private Guna.UI.WinForms.GunaLabel gunaLabel13;
+        private Guna.UI.WinForms.GunaWinSwitch gunaWinSwitch2;
+        private Guna.UI.WinForms.GunaLabel gunaLabel7;
+        private Guna.UI.WinForms.GunaLabel gunaLabel6;
+        private Guna.UI.WinForms.GunaLabel gunaLabel18;
+        private Guna.UI.WinForms.GunaLabel gunaLabel15;
+        private Guna.UI.WinForms.GunaLabel gunaLabel12;
+        private Guna.UI.WinForms.GunaLabel gunaLabel5;
+        private Guna.UI.WinForms.GunaGradientButton gunaGradientButton1;
+    }
+}
---
+++ InternetTest/UserControls/SettingsPages/Test.cs
@@ -0,0 +1,106 @@
+﻿using InternetTest.Classes;
+using InternetTest.Forms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace InternetTest.UserControls.SettingsPages
+{
+    public partial class Test : UserControl
+    {
+        public Test()
+        {
+            InitializeComponent();
+            LoadSettings();
+        }
+
+        private void gunaWinSwitch3_CheckedChanged(object sender, EventArgs e)
+        {
+            if (new Language().GetCode() == "fr-FR") // Si la langue est française
+            {
+                if (gunaWinSwitch3.Checked)
+                {
+                    gunaLabel17.Text = "Activé";
+                }
+                else
+                {
+                    gunaLabel17.Text = "Désactivé";
+                }
+            }
+            else if (new Language().GetCode() == "EN") // Si la langue est anglaise
+            {
+                if (gunaWinSwitch3.Checked)
+                {
+                    gunaLabel17.Text = "Enabled";
+                }
+                else
+                {
+                    gunaLabel17.Text = "Disabled";
+                }
+            }
+        }
+
+        private void gunaWinSwitch2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (new Language().GetCode() == "fr-FR") // Si la langue est française
+            {
+                if (gunaWinSwitch2.Checked)
+                {
+                    gunaLabel17.Text = "Activé";
+                }
+                else
+                {
+                    gunaLabel17.Text = "Désactivé";
+                }
+            }
+            else if (new Language().GetCode() == "EN") // Si la langue est anglaise
+            {
+                if (gunaWinSwitch2.Checked)
+                {
+                    gunaLabel17.Text = "Enabled";
+                }
+                else
+                {
+                    gunaLabel17.Text = "Disabled";
+                }
+            }
+        }
+
+        private void gunaLinkLabel2_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            new SelectDefaultSite(this).Show();
+        }
+
+        public void UpdateSite()
+        {
+            gunaLabel14.Text = Properties.Settings.Default.TestSite;
+            gunaLinkLabel2.Location = new Point(gunaLabel14.Width + 15, 116);
+        }
+
+        private void LoadSettings()
+        {
+            gunaWinSwitch2.Checked = Properties.Settings.Default.NotifyUpdate;
+            gunaWinSwitch3.Checked = Properties.Settings.Default.TestOnStart;
+
+            gunaLabel14.Text = Properties.Settings.Default.TestSite;
+            gunaLinkLabel2.Location = new Point(gunaLabel14.Width + 15, 116);
+
+            gunaComboBox2.Text = Properties.Settings.Default.MapsProvider;
+        }
+
+        private void gunaGradientButton1_Click(object sender, EventArgs e)
+        {
+            Properties.Settings.Default.MapsProvider = gunaComboBox2.Text;
+            Properties.Settings.Default.NotifyUpdate = gunaWinSwitch2.Checked; // Update
+            Properties.Settings.Default.TestOnStart = gunaWinSwitch3.Checked; // Update
+
+            Properties.Settings.Default.Save(); // Save
+        }
+    }
+}
