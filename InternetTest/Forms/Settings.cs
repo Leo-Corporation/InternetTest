bffadbcfa4d5503110eb4c53f51248aebcda429f
Léo Peyronnet <leo.peyronnet@outlook.fr>
2020-12-03 14:20:29 +0100

    Redesigned the Settings window (#156)

--- InternetTest/Forms/Settings.cs
+++ InternetTest/Forms/Settings.cs
@@ -20,6 +20,7 @@ namespace InternetTest.Forms
         {
             InitializeComponent();
             form1 = frm;
+            ChangePage(SettingsPage.Theme);
         }
 
         private void Settings_Load(object sender, EventArgs e)
@@ -27,9 +28,7 @@ namespace InternetTest.Forms
             Icon = new Branches().IconBranch(); // Met l'icône en foncion de la branche
             gunaPictureBox1.Image = new Branches().ImageBranch(); // Met l'image en fonction de la branche
             Guna.UI.Lib.GraphicsHelper.ShadowForm(this);
-            gunaLabel11.Visible = false;
             ChangeTheme();
-            LoadSettings();
         }
 
         private void gunaGradientButton2_Click(object sender, EventArgs e)
@@ -49,46 +48,9 @@ namespace InternetTest.Forms
 
         private void gunaGradientButton1_Click(object sender, EventArgs e)
         {
-            ChangeSettings();
+
             form1.ChangeTheme();
             Close();
-        }
-
-        private void ChangeSettings() // Appliquer les changements
-        {
-            Properties.Settings.Default.IsThemeDark = gunaWinSwitch1.Checked; // Mettre à jour le paramètre
-            Properties.Settings.Default.NotifyUpdate = gunaWinSwitch2.Checked; // Mettre à jour le paramètre
-            Properties.Settings.Default.TestOnStart = gunaWinSwitch3.Checked; // Mettre à jour le paramètre
-            if (gunaComboBox1.Text.Contains("fr-FR")) // Si la langue est française
-            {
-                Properties.Settings.Default.Language = "fr-FR";
-                Thread.CurrentThread.CurrentUICulture = new System.Globalization.CultureInfo("fr-FR");
-            }
-            else if (gunaComboBox1.Text.Contains("EN")) // Si la langue est anglaise
-            {
-                Properties.Settings.Default.Language = "EN";
-                Thread.CurrentThread.CurrentUICulture = new System.Globalization.CultureInfo("en-US");
-            }
-            Properties.Settings.Default.MapsProvider = gunaComboBox2.Text; // Mettre à jour le paramètre
-            Properties.Settings.Default.Save(); // Sauvegarder
-        }
-
-        private void LoadSettings() // Charger les paramètres et mettre à jour l'interface
-        {
-            gunaWinSwitch1.Checked = Properties.Settings.Default.IsThemeDark;
-            gunaWinSwitch2.Checked = Properties.Settings.Default.NotifyUpdate;
-            gunaWinSwitch3.Checked = Properties.Settings.Default.TestOnStart;
-            if (Properties.Settings.Default.Language == "fr-FR")
-            {
-                gunaComboBox1.Text = new Language().GetName("fr");
-            }
-            else if (Properties.Settings.Default.Language == "EN")
-            {
-                gunaComboBox1.Text = new Language().GetName("en");
-            }
-            gunaComboBox2.Text = Properties.Settings.Default.MapsProvider; // Mettre à jour le texte
-            gunaLabel14.Text = Properties.Settings.Default.TestSite;
-            gunaLinkLabel2.Location = new Point(gunaLabel14.Width + 15, 288);
         }
 
         private void ChangeTheme()
@@ -97,28 +59,6 @@ namespace InternetTest.Forms
             {
                 BackColor = Color.FromArgb(50, 50, 72);
                 gunaLabel1.ForeColor = Color.White;
-                gunaLabel2.ForeColor = Color.White;
-                gunaLabel3.ForeColor = Color.White;
-                gunaLabel4.ForeColor = Color.White;
-                gunaLabel5.ForeColor = Color.White;
-                gunaLabel6.ForeColor = Color.White;
-                gunaLabel7.ForeColor = Color.White;
-                gunaLabel8.ForeColor = Color.White;
-                gunaLabel9.ForeColor = Color.White;
-                gunaLabel10.ForeColor = Color.White;
-                gunaLabel11.ForeColor = Color.White;
-                gunaLabel12.ForeColor = Color.White;
-                gunaLabel13.ForeColor = Color.White;
-                gunaLabel14.ForeColor = Color.White;
-                gunaLabel15.ForeColor = Color.White;
-                gunaLabel16.ForeColor = Color.White;
-                gunaLabel17.ForeColor = Color.White;
-                gunaLabel18.ForeColor = Color.White;
-                gunaLabel19.ForeColor = Color.White;
-                gunaComboBox1.ForeColor = Color.White;
-                gunaComboBox2.ForeColor = Color.White;
-                gunaComboBox1.BaseColor = Color.FromArgb(50, 50, 72);
-                gunaComboBox2.BaseColor = Color.FromArgb(50, 50, 72);
                 gunaAdvenceTileButton1.Image = Properties.Resources.icons8_delete_32px;
                 gunaAdvenceTileButton2.Image = Properties.Resources.icons8_subtract_100px;
             }
@@ -126,173 +66,68 @@ namespace InternetTest.Forms
             {
                 BackColor = Color.White;
                 gunaLabel1.ForeColor = Color.Black;
-                gunaLabel2.ForeColor = Color.Black;
-                gunaLabel3.ForeColor = Color.Black;
-                gunaLabel4.ForeColor = Color.Black;
-                gunaLabel5.ForeColor = Color.Black;
-                gunaLabel6.ForeColor = Color.Black;
-                gunaLabel7.ForeColor = Color.Black;
-                gunaLabel8.ForeColor = Color.Black;
-                gunaLabel9.ForeColor = Color.Black;
-                gunaLabel10.ForeColor = Color.Black;
-                gunaLabel11.ForeColor = Color.Black;
-                gunaLabel12.ForeColor = Color.Black;
-                gunaLabel13.ForeColor = Color.Black;
-                gunaLabel14.ForeColor = Color.Black;
-                gunaLabel15.ForeColor = Color.Black;
-                gunaLabel16.ForeColor = Color.Black;
-                gunaLabel17.ForeColor = Color.Black;
-                gunaLabel18.ForeColor = Color.Black;
-                gunaLabel19.ForeColor = Color.Black;
-                gunaComboBox1.ForeColor = Color.Black;
-                gunaComboBox2.ForeColor = Color.Black;
-                gunaComboBox1.BaseColor = Color.White;
-                gunaComboBox2.BaseColor = Color.White;
                 gunaAdvenceTileButton1.Image = Properties.Resources.icons8_delete_100px_1;
                 gunaAdvenceTileButton2.Image = Properties.Resources.icons8_subtract_100px_1;
             }
         }
 
-        private void gunaWinSwitch1_CheckedChanged(object sender, EventArgs e)
+        private void gunaGradientButton3_Click(object sender, EventArgs e)
         {
-            if (new Language().GetCode() == "fr-FR")
+            ChangePage(SettingsPage.Theme); // Change page
+        }
+
+        private void gunaGradientButton4_Click(object sender, EventArgs e)
+        {
+            ChangePage(SettingsPage.Language); // Change page
+        }
+
+        private void gunaGradientButton5_Click(object sender, EventArgs e)
+        {
+            ChangePage(SettingsPage.Test); // Change page
+        }
+
+        private void gunaGradientButton6_Click(object sender, EventArgs e)
+        {
+            ChangePage(SettingsPage.Data); // Change page
+        }
+
+        private void ChangePage(SettingsPage settingsPage)
+        {
+            switch (settingsPage)
             {
-                if (gunaWinSwitch1.Checked)
-                {
-                    gunaLabel4.Text = "Activé";
-                }
-                else
-                {
-                    gunaLabel4.Text = "Désactivé";
-                }
-            }
-            else if (new Language().GetCode() == "EN")
-            {
-                if (gunaWinSwitch1.Checked)
-                {
-                    gunaLabel4.Text = "Enabled";
-                }
-                else
-                {
-                    gunaLabel4.Text = "Disabled";
-                }
+                case SettingsPage.Data:
+                    data1.Visible = true; // Show
+                    languages1.Visible = false; // Hide
+                    test1.Visible = false; // Hide
+                    theme1.Visible = false; // Hide
+                    break;
+                case SettingsPage.Theme:
+                    data1.Visible = false; // Hide
+                    languages1.Visible = false; // Hide
+                    test1.Visible = false; // Hide
+                    theme1.Visible = true; // Show
+                    break;
+                case SettingsPage.Test:
+                    data1.Visible = false; // Hide
+                    languages1.Visible = false; // Hide
+                    test1.Visible = true; // Show
+                    theme1.Visible = false; // Hide
+                    break;
+                case SettingsPage.Language:
+                    data1.Visible = false; // Hide
+                    languages1.Visible = true; // Show
+                    test1.Visible = false; // Hide
+                    theme1.Visible = false; // Hide
+                    break;
             }
         }
 
-        private void gunaWinSwitch2_CheckedChanged(object sender, EventArgs e)
+        enum SettingsPage
         {
-            if (new Language().GetCode() == "fr-FR") // Si la langue est française
-            {
-                if (gunaWinSwitch2.Checked)
-                {
-                    gunaLabel7.Text = "Activé";
-                }
-                else
-                {
-                    gunaLabel7.Text = "Désactivé";
-                }
-            }
-            else if (new Language().GetCode() == "EN") // Si la langue est anglaise
-            {
-                if (gunaWinSwitch2.Checked)
-                {
-                    gunaLabel7.Text = "Enabled";
-                }
-                else
-                {
-                    gunaLabel7.Text = "Disabled";
-                }
-            }
-        }
-
-        private void gunaLinkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
-        {
-            string promptBeforeRemove = "";
-
-            if (gunaComboBox1.Text.Contains("fr-FR")) // If the lang is french
-            {
-                promptBeforeRemove = "Êtes-vous sûr de vouloir effacer toutes les données d'InternetTest ?";
-            }
-            else if (gunaComboBox1.Text.Contains("EN")) // If the lang is english
-            {
-                promptBeforeRemove = "Are you sure you wanna erase all InternetTest's data?";
-            }
-
-            if (MessageBox.Show(promptBeforeRemove, "Confirmation", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
-            {
-                Properties.Settings.Default.Reset();
-                Properties.Settings.Default.Save();
-                UpdateSite();
-                if (gunaComboBox1.Text.Contains("fr-FR"))
-                {
-                    gunaComboBox1.Text = "Français (fr-FR)";
-                    gunaWinSwitch1.Checked = false;
-                    gunaWinSwitch2.Checked = true;
-                    MessageBox.Show("Les données ont été effacées.", "Terminé", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                }
-                else if (gunaComboBox1.Text.Contains("EN"))
-                {
-                    gunaComboBox1.Text = "French (fr-FR)";
-                    gunaWinSwitch1.Checked = false;
-                    gunaWinSwitch2.Checked = true;
-                    MessageBox.Show("Data has been removed.", "Done", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                }
-                gunaComboBox2.Text = Properties.Settings.Default.MapsProvider; // Mettre à jour le texte
-            }
-        }
-
-        private void gunaComboBox1_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            if (new Language().GetCode() == "fr-FR")
-            {
-                if (gunaComboBox1.Text != new Language().GetName("fr"))
-                {
-                    gunaLabel11.Visible = true;
-                }
-            }else if (new Language().GetCode() == "EN")
-            {
-                if (gunaComboBox1.Text != new Language().GetName("en"))
-                {
-                    gunaLabel11.Visible = true;
-                }
-            }
-        }
-
-        private void gunaLinkLabel2_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
-        {
-            new SelectDefaultSite(this).Show();
-        }
-
-        public void UpdateSite()
-        {
-            gunaLabel14.Text = Properties.Settings.Default.TestSite;
-            gunaLinkLabel2.Location = new Point(gunaLabel14.Width + 15, 288);
-        }
-
-        private void gunaWinSwitch3_CheckedChanged(object sender, EventArgs e)
-        {
-            if (new Language().GetCode() == "fr-FR") // Si la langue est française
-            {
-                if (gunaWinSwitch3.Checked)
-                {
-                    gunaLabel17.Text = "Activé";
-                }
-                else
-                {
-                    gunaLabel17.Text = "Désactivé";
-                }
-            }
-            else if (new Language().GetCode() == "EN") // Si la langue est anglaise
-            {
-                if (gunaWinSwitch3.Checked)
-                {
-                    gunaLabel17.Text = "Enabled";
-                }
-                else
-                {
-                    gunaLabel17.Text = "Disabled";
-                }
-            }
+            Theme,
+            Language,
+            Test,
+            Data
         }
     }
 }
