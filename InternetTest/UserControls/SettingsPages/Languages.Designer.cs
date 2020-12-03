e50a7abde294513617c66efa9e317728c3fa30d6
Léo Peyronnet <leo.peyronnet@outlook.fr>
2020-12-03 14:19:06 +0100

    Added the "Languages" page (#156)

---
+++ InternetTest/UserControls/SettingsPages/Languages.Designer.cs
@@ -0,0 +1,154 @@
+﻿
+namespace InternetTest.UserControls.SettingsPages
+{
+    partial class Languages
+    {
+        /// <summary> 
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur de composants
+
+        /// <summary> 
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas 
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gunaComboBox1 = new Guna.UI.WinForms.GunaComboBox();
+            this.gunaLabel11 = new Guna.UI.WinForms.GunaLabel();
+            this.gunaLabel9 = new Guna.UI.WinForms.GunaLabel();
+            this.gunaLabel8 = new Guna.UI.WinForms.GunaLabel();
+            this.gunaGradientButton1 = new Guna.UI.WinForms.GunaGradientButton();
+            this.SuspendLayout();
+            // 
+            // gunaComboBox1
+            // 
+            this.gunaComboBox1.BackColor = System.Drawing.Color.Transparent;
+            this.gunaComboBox1.BaseColor = System.Drawing.Color.White;
+            this.gunaComboBox1.BorderColor = System.Drawing.Color.Silver;
+            this.gunaComboBox1.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed;
+            this.gunaComboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.gunaComboBox1.FocusedColor = System.Drawing.Color.Empty;
+            this.gunaComboBox1.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold);
+            this.gunaComboBox1.ForeColor = System.Drawing.Color.Black;
+            this.gunaComboBox1.FormattingEnabled = true;
+            this.gunaComboBox1.Items.AddRange(new object[] {
+            "Français (fr-FR)",
+            "Anglais (EN)"});
+            this.gunaComboBox1.Location = new System.Drawing.Point(17, 60);
+            this.gunaComboBox1.Name = "gunaComboBox1";
+            this.gunaComboBox1.OnHoverItemBaseColor = System.Drawing.Color.RoyalBlue;
+            this.gunaComboBox1.OnHoverItemForeColor = System.Drawing.Color.White;
+            this.gunaComboBox1.Radius = 6;
+            this.gunaComboBox1.Size = new System.Drawing.Size(181, 28);
+            this.gunaComboBox1.TabIndex = 8;
+            this.gunaComboBox1.SelectedIndexChanged += new System.EventHandler(this.gunaComboBox1_SelectedIndexChanged);
+            // 
+            // gunaLabel11
+            // 
+            this.gunaLabel11.AutoSize = true;
+            this.gunaLabel11.BackColor = System.Drawing.Color.Transparent;
+            this.gunaLabel11.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold);
+            this.gunaLabel11.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.gunaLabel11.Location = new System.Drawing.Point(208, 56);
+            this.gunaLabel11.Name = "gunaLabel11";
+            this.gunaLabel11.Size = new System.Drawing.Size(176, 34);
+            this.gunaLabel11.TabIndex = 5;
+            this.gunaLabel11.Text = "Redémarrez le logiciel pour \r\nappliquer les changements";
+            // 
+            // gunaLabel9
+            // 
+            this.gunaLabel9.AutoSize = true;
+            this.gunaLabel9.BackColor = System.Drawing.Color.Transparent;
+            this.gunaLabel9.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold);
+            this.gunaLabel9.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.gunaLabel9.Location = new System.Drawing.Point(14, 40);
+            this.gunaLabel9.Name = "gunaLabel9";
+            this.gunaLabel9.Size = new System.Drawing.Size(141, 17);
+            this.gunaLabel9.TabIndex = 6;
+            this.gunaLabel9.Text = "Choisissez une langue";
+            // 
+            // gunaLabel8
+            // 
+            this.gunaLabel8.AutoSize = true;
+            this.gunaLabel8.BackColor = System.Drawing.Color.Transparent;
+            this.gunaLabel8.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Bold);
+            this.gunaLabel8.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.gunaLabel8.Location = new System.Drawing.Point(12, 13);
+            this.gunaLabel8.Name = "gunaLabel8";
+            this.gunaLabel8.Size = new System.Drawing.Size(80, 28);
+            this.gunaLabel8.TabIndex = 7;
+            this.gunaLabel8.Text = "Langue";
+            // 
+            // gunaGradientButton1
+            // 
+            this.gunaGradientButton1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.gunaGradientButton1.Animated = true;
+            this.gunaGradientButton1.AnimationHoverSpeed = 0.07F;
+            this.gunaGradientButton1.AnimationSpeed = 0.03F;
+            this.gunaGradientButton1.BackColor = System.Drawing.Color.Transparent;
+            this.gunaGradientButton1.BaseColor1 = System.Drawing.Color.DeepSkyBlue;
+            this.gunaGradientButton1.BaseColor2 = System.Drawing.Color.RoyalBlue;
+            this.gunaGradientButton1.BorderColor = System.Drawing.Color.Black;
+            this.gunaGradientButton1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.gunaGradientButton1.DialogResult = System.Windows.Forms.DialogResult.None;
+            this.gunaGradientButton1.FocusedColor = System.Drawing.Color.Empty;
+            this.gunaGradientButton1.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Bold);
+            this.gunaGradientButton1.ForeColor = System.Drawing.Color.White;
+            this.gunaGradientButton1.Image = global::InternetTest.Properties.Resources.checkmark;
+            this.gunaGradientButton1.ImageSize = new System.Drawing.Size(20, 20);
+            this.gunaGradientButton1.Location = new System.Drawing.Point(221, 362);
+            this.gunaGradientButton1.Name = "gunaGradientButton1";
+            this.gunaGradientButton1.OnHoverBaseColor1 = System.Drawing.Color.DodgerBlue;
+            this.gunaGradientButton1.OnHoverBaseColor2 = System.Drawing.Color.MediumBlue;
+            this.gunaGradientButton1.OnHoverBorderColor = System.Drawing.Color.Black;
+            this.gunaGradientButton1.OnHoverForeColor = System.Drawing.Color.White;
+            this.gunaGradientButton1.OnHoverImage = null;
+            this.gunaGradientButton1.OnPressedColor = System.Drawing.Color.Black;
+            this.gunaGradientButton1.Radius = 6;
+            this.gunaGradientButton1.Size = new System.Drawing.Size(127, 33);
+            this.gunaGradientButton1.TabIndex = 10;
+            this.gunaGradientButton1.Text = "Appliquer";
+            this.gunaGradientButton1.Click += new System.EventHandler(this.gunaGradientButton1_Click);
+            // 
+            // Languages
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.Controls.Add(this.gunaGradientButton1);
+            this.Controls.Add(this.gunaComboBox1);
+            this.Controls.Add(this.gunaLabel11);
+            this.Controls.Add(this.gunaLabel9);
+            this.Controls.Add(this.gunaLabel8);
+            this.Name = "Languages";
+            this.Size = new System.Drawing.Size(568, 411);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Guna.UI.WinForms.GunaComboBox gunaComboBox1;
+        private Guna.UI.WinForms.GunaLabel gunaLabel11;
+        private Guna.UI.WinForms.GunaLabel gunaLabel9;
+        private Guna.UI.WinForms.GunaLabel gunaLabel8;
+        private Guna.UI.WinForms.GunaGradientButton gunaGradientButton1;
+    }
+}
---
+++ InternetTest/UserControls/SettingsPages/Languages.cs
@@ -0,0 +1,69 @@
+﻿using InternetTest.Classes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace InternetTest.UserControls.SettingsPages
+{
+    public partial class Languages : UserControl
+    {
+        public Languages()
+        {
+            InitializeComponent();
+            LoadSettings(); // Load the settings
+        }
+
+        private void gunaComboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (new Language().GetCode() == "fr-FR")
+            {
+                if (gunaComboBox1.Text != new Language().GetName("fr"))
+                {
+                    gunaLabel11.Visible = true;
+                }
+            }
+            else if (new Language().GetCode() == "EN")
+            {
+                if (gunaComboBox1.Text != new Language().GetName("en"))
+                {
+                    gunaLabel11.Visible = true;
+                }
+            }
+        }
+
+        private void gunaGradientButton1_Click(object sender, EventArgs e)
+        {
+            if (gunaComboBox1.Text.Contains("fr-FR")) // Si la langue est française
+            {
+                Properties.Settings.Default.Language = "fr-FR";
+                Thread.CurrentThread.CurrentUICulture = new System.Globalization.CultureInfo("fr-FR");
+            }
+            else if (gunaComboBox1.Text.Contains("EN")) // Si la langue est anglaise
+            {
+                Properties.Settings.Default.Language = "EN";
+                Thread.CurrentThread.CurrentUICulture = new System.Globalization.CultureInfo("en-US");
+            }
+            Properties.Settings.Default.Save(); // Save
+        }
+
+        private void LoadSettings()
+        {
+            gunaLabel11.Visible = false; // Hide
+            if (Properties.Settings.Default.Language == "fr-FR")
+            {
+                gunaComboBox1.Text = new Language().GetName("fr");
+            }
+            else if (Properties.Settings.Default.Language == "EN")
+            {
+                gunaComboBox1.Text = new Language().GetName("en");
+            }
+        }
+    }
+}
