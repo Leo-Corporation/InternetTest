8ecdafbe623f132b7c3181309499a94c45eeb39b
Léo Peyronnet <leo.peyronnet@outlook.fr>
2020-12-03 14:18:40 +0100

    Added the "Data" page (#156)

---
+++ InternetTest/UserControls/SettingsPages/Data.Designer.cs
@@ -0,0 +1,82 @@
+﻿
+namespace InternetTest.UserControls.SettingsPages
+{
+    partial class Data
+    {
+        /// <summary> 
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur de composants
+
+        /// <summary> 
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas 
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gunaLinkLabel1 = new Guna.UI.WinForms.GunaLinkLabel();
+            this.gunaLabel10 = new Guna.UI.WinForms.GunaLabel();
+            this.SuspendLayout();
+            // 
+            // gunaLinkLabel1
+            // 
+            this.gunaLinkLabel1.AutoSize = true;
+            this.gunaLinkLabel1.BackColor = System.Drawing.Color.Transparent;
+            this.gunaLinkLabel1.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold);
+            this.gunaLinkLabel1.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.gunaLinkLabel1.LinkColor = System.Drawing.Color.RoyalBlue;
+            this.gunaLinkLabel1.Location = new System.Drawing.Point(14, 41);
+            this.gunaLinkLabel1.Name = "gunaLinkLabel1";
+            this.gunaLinkLabel1.Size = new System.Drawing.Size(127, 17);
+            this.gunaLinkLabel1.TabIndex = 8;
+            this.gunaLinkLabel1.TabStop = true;
+            this.gunaLinkLabel1.Text = "Effacer les données";
+            this.gunaLinkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.gunaLinkLabel1_LinkClicked);
+            // 
+            // gunaLabel10
+            // 
+            this.gunaLabel10.AutoSize = true;
+            this.gunaLabel10.BackColor = System.Drawing.Color.Transparent;
+            this.gunaLabel10.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Bold);
+            this.gunaLabel10.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.gunaLabel10.Location = new System.Drawing.Point(12, 13);
+            this.gunaLabel10.Name = "gunaLabel10";
+            this.gunaLabel10.Size = new System.Drawing.Size(94, 28);
+            this.gunaLabel10.TabIndex = 7;
+            this.gunaLabel10.Text = "Données";
+            // 
+            // Data
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.Controls.Add(this.gunaLinkLabel1);
+            this.Controls.Add(this.gunaLabel10);
+            this.Name = "Data";
+            this.Size = new System.Drawing.Size(568, 411);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Guna.UI.WinForms.GunaLinkLabel gunaLinkLabel1;
+        private Guna.UI.WinForms.GunaLabel gunaLabel10;
+    }
+}
---
+++ InternetTest/UserControls/SettingsPages/Data.cs
@@ -0,0 +1,51 @@
+﻿using InternetTest.Classes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace InternetTest.UserControls.SettingsPages
+{
+    public partial class Data : UserControl
+    {
+        public Data()
+        {
+            InitializeComponent();
+        }
+
+        private void gunaLinkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            string promptBeforeRemove = "";
+
+            if (new Language().GetCode() == "fr-FR") // If the lang is french
+            {
+                promptBeforeRemove = "Êtes-vous sûr de vouloir effacer toutes les données d'InternetTest ?";
+            }
+            else if (new Language().GetCode() == "EN") // If the lang is english
+            {
+                promptBeforeRemove = "Are you sure you wanna erase all InternetTest's data?";
+            }
+
+            if (MessageBox.Show(promptBeforeRemove, "Confirmation", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                Properties.Settings.Default.Reset();
+                Properties.Settings.Default.Save();
+                if (new Language().GetCode() == "fr-FR")
+                {
+                    
+                    MessageBox.Show("Les données ont été effacées.", "Terminé", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else if (new Language().GetCode() == "EN")
+                {
+                   
+                    MessageBox.Show("Data has been removed.", "Done", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+        }
+    }
+}
