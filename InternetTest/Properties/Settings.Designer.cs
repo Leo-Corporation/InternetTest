923c11365679a85f0d059debaa4780c41f28195e
Léo Peyronnet <leo.peyronnet@outlook.fr>
2021-02-07 10:41:25 +0100

    Changed the default map provider (#172)

--- InternetTest/Properties/Settings.Designer.cs
+++ InternetTest/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace InternetTest.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.6.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.8.1.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -85,7 +85,7 @@ namespace InternetTest.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("Bing Maps")]
+        [global::System.Configuration.DefaultSettingValueAttribute("OpenStreetMap")]
         public string MapsProvider {
             get {
                 return ((string)(this["MapsProvider"]));
