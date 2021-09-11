3c3c1cd542f13112b600c13c4047791aa1ad3087
Léo Peyronnet <leo.peyronnet@outlook.fr>
2021-09-11 09:04:16 +0200

    Version 5.6.0.2109-rc1

--- InternetTest/InternetTest/Classes/Global.cs
+++ InternetTest/InternetTest/Classes/Global.cs
@@ -70,7 +70,7 @@ namespace InternetTest.Classes
 		/// <summary>
 		/// The current version of InternetTest.
 		/// </summary>
-		public static string Version => "5.6.0.2109-pre1";
+		public static string Version => "5.6.0.2109-rc1";
 
 		/// <summary>
 		/// List of the available languages.
