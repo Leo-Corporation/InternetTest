487b0afd54bd6864cd6ac52abedd84c21f1e8665
Léo Peyronnet <leo.peyronnet@outlook.fr>
2021-11-19 16:55:27 +0100

    Added a point corresponding to the location on Google Maps (#267)

--- InternetTest/InternetTest/Pages/LocalizeIPPage.xaml.cs
+++ InternetTest/InternetTest/Pages/LocalizeIPPage.xaml.cs
@@ -97,7 +97,7 @@ namespace InternetTest.Pages
 				switch (Global.Settings.MapProvider)
 				{
 					case MapProviders.BingMaps: Global.OpenLinkInBrowser($"https://www.bing.com/maps?q={lat} {lon}"); break;
-					case MapProviders.GoogleMaps: Global.OpenLinkInBrowser($"https://www.google.com/maps/place/{lat},{lon}"); break;
+					case MapProviders.GoogleMaps: Global.OpenLinkInBrowser("https://www.google.com/maps/dir/{lat},{lon}//@{lat},{lon}"); break;
 					case MapProviders.OpenStreetMap: Global.OpenLinkInBrowser($"https://www.openstreetmap.org/#map=12/{lat}/{lon}"); break;
 					case MapProviders.Yandex: Global.OpenLinkInBrowser($"https://yandex.com/maps/?ll={lon}%2C{lat}&z=12"); break;
 					case MapProviders.HereWeGo: Global.OpenLinkInBrowser($"https://wego.here.com/directions/mix/{lat},{lon}/?map={lat},{lon},12"); break;
