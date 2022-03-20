7323947cb361654fe18c74860732abe77e0241a9
Léo Peyronnet <leo.peyronnet@outlook.fr>
2022-03-20 15:35:30 +0100

    Version 6.1.0.2203-pre1

--- InternetTest/InternetTest/Classes/Global.cs
+++ InternetTest/InternetTest/Classes/Global.cs
@@ -68,7 +68,7 @@ public static class Global
 	/// <summary>
 	/// The current version of InternetTest.
 	/// </summary>
-	public static string Version => "6.0.1.2202";
+	public static string Version => "6.1.0.2203-pre1";
 
 	/// <summary>
 	/// List of the available languages.
