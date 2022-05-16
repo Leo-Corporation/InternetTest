836ed795cb8d7aaad42ca49982298be4aca04646
Léo Peyronnet <leo.peyronnet@outlook.fr>
2022-05-16 15:13:55 +0200

    Code cleanup

--- InternetTest/InternetTest/UserControls/IpHistoryItem.xaml.cs
+++ InternetTest/InternetTest/UserControls/IpHistoryItem.xaml.cs
@@ -64,7 +64,7 @@ public partial class IpHistoryItem : UserControl
 	{
 		ParentElement.Children.Remove(this); // Remove the item from the parent element
 		Global.LocatedIPs.Remove(IPInfo.Query);
-		
+
 		if (ParentElement.Children.Count == 0) // If there are no more items in the parent element
 		{
 			Global.LocalizeIPPage.HistoryBtn_Click(this, null); // Hide the history
