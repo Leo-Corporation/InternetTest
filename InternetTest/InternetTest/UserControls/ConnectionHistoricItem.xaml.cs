8f1e6461509e113521bf7ed9619c682142a88cdb
Léo Peyronnet <leo.peyronnet@outlook.fr>
2022-07-06 09:49:14 +0200

    Code cleanup

--- InternetTest/InternetTest/UserControls/ConnectionHistoricItem.xaml.cs
+++ InternetTest/InternetTest/UserControls/ConnectionHistoricItem.xaml.cs
@@ -22,7 +22,6 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE. 
 */
 using InternetTest.Classes;
-using System;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
