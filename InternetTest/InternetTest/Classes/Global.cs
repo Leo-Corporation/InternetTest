6d73cd519fc19a1e128708fdbb558d64c946733f
Léo Peyronnet <leo.peyronnet@outlook.fr>
2022-07-07 08:57:20 +0200

    Version 6.3.1.2207

--- InternetTest/InternetTest/Classes/Global.cs
+++ InternetTest/InternetTest/Classes/Global.cs
@@ -68,7 +68,7 @@ public static class Global
 	/// <summary>
 	/// The current version of InternetTest.
 	/// </summary>
-	public static string Version => "6.3.0.2207";
+	public static string Version => "6.3.1.2207";
 
 	/// <summary>
 	/// List of the available languages.
