143b8030e948c735889a5093faa85ab5aa001891
Léo Peyronnet <leo.peyronnet@outlook.fr>
2022-08-19 15:32:31 +0200

    Added History classes (#353)

---
+++ InternetTest/InternetTest/Classes/History.cs
@@ -0,0 +1,60 @@
+﻿/*
+MIT License
+
+Copyright (c) Léo Corporation
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE. 
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InternetTest.Classes;
+
+public class History
+{
+	public List<StatusHistory> StatusHistory { get; set; }
+	public List<StatusHistory> DownDetectorHistory { get; set; }
+	public History()
+	{
+		StatusHistory = new();
+		DownDetectorHistory = new();
+	}
+}
+
+public class HistoryItem
+{
+	public string Content { get; set; }
+	public HistoryItem(string content)
+	{
+		Content = content;
+	}
+}
+
+public class StatusHistory : HistoryItem
+{
+	public string Icon { get; set; }
+
+	public StatusHistory(string content, string icon) : base(content)
+	{
+		Icon = icon;
+	}
+}
