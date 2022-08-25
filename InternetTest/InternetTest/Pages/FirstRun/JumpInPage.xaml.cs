23f9c16e39c10f77cb1056d309bbf493d03844bd
Léo Peyronnet <leo.peyronnet@outlook.fr>
2022-08-25 10:57:20 +0200

    Code cleanup

--- InternetTest/InternetTest/Pages/FirstRun/JumpInPage.xaml.cs
+++ InternetTest/InternetTest/Pages/FirstRun/JumpInPage.xaml.cs
@@ -24,21 +24,9 @@ SOFTWARE.
 
 using InternetTest.Classes;
 using LeoCorpLibrary;
-using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace InternetTest.Pages.FirstRun;
 /// <summary>
