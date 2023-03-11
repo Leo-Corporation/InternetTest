68c4632b5516a307bf88a5d729e96c3311be6805
Léo Peyronnet <leo.peyronnet@outlook.fr>
2023-03-11 10:28:57 +0100

    Added the possibility to copy the details in My IP (#404)

--- InternetTest/InternetTest/Pages/MyIpPage.xaml.cs
+++ InternetTest/InternetTest/Pages/MyIpPage.xaml.cs
@@ -187,4 +187,9 @@ public partial class MyIpPage : Page
 		}
 		catch { }
 	}
+
+	private void TextBlock_MouseLeftButtonUp(object sender, System.Windows.Input.MouseButtonEventArgs e)
+	{
+		Clipboard.SetText(((TextBlock)sender).Text);
+	}
 }
