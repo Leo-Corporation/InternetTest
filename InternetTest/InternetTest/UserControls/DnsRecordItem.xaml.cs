7dd678188e3dc7682e11e06b4341ac4ae24a6125
Léo Peyronnet <leo.peyronnet@outlook.fr>
2023-04-19 11:02:40 +0200

    Added the DNS record item (#418)

---
+++ InternetTest/InternetTest/UserControls/DnsRecordItem.xaml.cs
@@ -0,0 +1,53 @@
+﻿/*
+MIT License
+
+Copyright (c) Léo Corporation
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE. 
+*/
+
+using System.Windows;
+using System.Windows.Controls;
+
+namespace InternetTest.UserControls;
+/// <summary>
+/// Interaction logic for DnsRecordItem.xaml
+/// </summary>
+public partial class DnsRecordItem : UserControl
+{
+	private string Type { get; init; }
+	private string RecordContent { get; init; }
+	public DnsRecordItem(string type, string content)
+	{
+		InitializeComponent();
+		Type = type;
+		RecordContent = content;
+		InitUI();
+	}
+	private void InitUI()
+	{
+		RecordTypeTxt.Text = Type;
+		RecordContentTxt.Text = RecordContent;
+	}
+
+	private void CopyBtn_Click(object sender, RoutedEventArgs e)
+	{
+		Clipboard.SetText(RecordContent);
+	}
+}
