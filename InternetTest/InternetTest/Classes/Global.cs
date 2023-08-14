436141f6ae5d95bc388d42ad223d8af9a5fc3c61
Léo Peyronnet <leo.peyronnet@outlook.fr>
2023-08-14 11:42:44 +0200

    Version 7.7.0.2308-pre1

--- InternetTest/InternetTest/Classes/Global.cs
+++ InternetTest/InternetTest/Classes/Global.cs
@@ -47,10 +47,10 @@ public static class Global
 #if NIGHTLY
 	private static DateTime Date => System.IO.File.GetLastWriteTime(System.Reflection.Assembly.GetEntryAssembly().Location);
 
-	public static string Version => $"7.6.2.2307-nightly{Date:yyMM.dd@HHmm}";
+	public static string Version => $"7.7.0.2308-nightly{Date:yyMM.dd@HHmm}";
 
 #else
-	public static string Version => "7.6.2.2307";
+	public static string Version => "7.7.0.2308-pre1";
 #endif
 	public static string LastVersionLink => "https://raw.githubusercontent.com/Leo-Corporation/LeoCorp-Docs/master/Liens/Update%20System/InternetTest/7.0/Version.txt";
 	public static bool IsConfidentialModeEnabled { get; set; } = false;
