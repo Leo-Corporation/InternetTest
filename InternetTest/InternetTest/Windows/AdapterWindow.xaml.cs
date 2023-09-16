e900c7892c6e714c219e72dc887ffc27a98e54b3
Léo Peyronnet <leo.peyronnet@outlook.fr>
2023-09-16 12:02:30 +0200

    Added details popup (#481)

---
+++ InternetTest/InternetTest/Windows/AdapterWindow.xaml.cs
@@ -0,0 +1,89 @@
+﻿/*
+MIT License
+
+Copyright (c) Léo Corporation
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE. 
+*/
+
+using InternetTest.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace InternetTest.Windows
+{
+	/// <summary>
+	/// Interaction logic for AdapterWindow.xaml
+	/// </summary>
+	public partial class AdapterWindow : Window
+	{
+		public AdapterInfo AdapterInfo { get; }
+		public AdapterWindow(AdapterInfo adapterInfo)
+		{
+			InitializeComponent();
+			AdapterInfo = adapterInfo;
+			InitUI();
+		}
+
+		private void InitUI()
+		{
+			NameTxt.Text = AdapterInfo.Name;
+			InfoTxt.Text = AdapterInfo.ToFormattedString();
+			HeadTxt.Text = $"{Properties.Resources.Name}\n" +
+				$"{Properties.Resources.InterfaceType}\n" +
+				$"{Properties.Resources.Status}\n" +
+				$"{Properties.Resources.IpVersion}\n" +
+				$"{Properties.Resources.DNSSuffix}\n" +
+				$"{Properties.Resources.MTU}\n" +
+				$"{Properties.Resources.DnsEnabled}\n" +
+				$"{Properties.Resources.DnsDynamicConfigured}\n" +
+				$"{Properties.Resources.ReceiveOnly}\n" +
+				$"{Properties.Resources.Multicast}\n" +
+				$"{Properties.Resources.Speed}\n" +
+				$"{Properties.Resources.TotalBytesReceived}\n" +
+				$"{Properties.Resources.TotalBytesSent}\n" +
+				$"{Properties.Resources.IncomingPacketsDiscarded}\n" +
+				$"{Properties.Resources.IncomingPacketsWithErrors}\n" +
+				$"{Properties.Resources.IncomingUnknownProtocolPackets}\n" +
+				$"{Properties.Resources.NonUnicastPacketsReceived}\n" +
+				$"{Properties.Resources.NonUnicastPacketsSent}\n" +
+				$"{Properties.Resources.OutgoingPacketsDiscarded}\n" +
+				$"{Properties.Resources.OutgoingPacketsWithErrors}\n" +
+				$"{Properties.Resources.OutputQueueLength}\n" +
+				$"{Properties.Resources.UnicastPacketsReceived}\n" +
+				$"{Properties.Resources.UnicastPacketsSent}";
+		}
+
+		private void CloseBtn_Click(object sender, RoutedEventArgs e)
+		{
+			Close();
+		}
+	}
+}
