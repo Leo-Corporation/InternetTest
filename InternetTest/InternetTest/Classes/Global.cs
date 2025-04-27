525263b10e3b917f8cf37dc1c0419561374b56d3
Léo Peyronnet <leo.peyronnet@outlook.fr>
2025-04-27 12:26:15 +0200

    Version 8.9.0.2504-rc1

--- InternetTest/InternetTest/Classes/Global.cs
+++ InternetTest/InternetTest/Classes/Global.cs
@@ -51,7 +51,7 @@ public static class Global
 	public static string Version => $"8.9.0.2504-nightly{Date:yyMM.dd@HHmm}";
 
 #else
-	public static string Version => "8.9.0.2504-pre1";
+	public static string Version => "8.9.0.2504-rc1";
 #endif
 	public static string LastVersionLink => "https://raw.githubusercontent.com/Leo-Corporation/LeoCorp-Docs/master/Liens/Update%20System/InternetTest/7.0/Version.txt";
 
