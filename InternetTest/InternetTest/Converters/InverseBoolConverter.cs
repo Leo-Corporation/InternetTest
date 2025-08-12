b9e520456d0849501b8392bb27c75ae27840c709
Léo Peyronnet <leo.peyronnet@outlook.fr>
2025-08-12 14:15:21 +0200

    Added Converters for Settings page (#670)

--- InternetTest/InternetTest/Converters/InverseBoolConverter.cs
+++ InternetTest/InternetTest/Converters/InverseBoolConverter.cs
@@ -25,12 +25,23 @@ using System.Globalization;
 using System.Windows.Data;
 
 namespace InternetTest.Converters;
-
-public class ThemeToBoolConverter : IValueConverter
+public class InverseBoolConverter : IValueConverter
 {
 	public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
-		=> value.Equals(parameter);
+	{
+		if (value is bool b)
+		{
+			return !b;
+		}
+		return false;
+	}
 
 	public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
-		=> (bool)value ? parameter : Binding.DoNothing;
+	{
+		if (value is bool b)
+		{
+			return !b;
+		}
+		return false;
+	}
 }
