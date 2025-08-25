2af5a88817a19d94929bc683ba5ff946359c7356
Léo Peyronnet <leo.peyronnet@outlook.fr>
2025-08-25 14:34:23 +0200

    Added the possibility to get DNS Records (#679)

--- InternetTest/InternetTest/ViewModels/DnsToolsPageViewModel.cs
+++ InternetTest/InternetTest/ViewModels/DnsToolsPageViewModel.cs
@@ -25,8 +25,10 @@ using DnsClient;
 using DnsClient.Protocol;
 using InternetTest.Commands;
 using InternetTest.Models;
+using InternetTest.ViewModels.Components;
 using System.Collections.ObjectModel;
 using System.Net;
+using System.Windows;
 using System.Windows.Input;
 using Whois;
 
@@ -35,6 +37,9 @@ public class DnsToolsPageViewModel : ViewModelBase
 {
 	private ObservableCollection<DnsRecordTabBtnViewModel> _dnsTabs = [];
 	public ObservableCollection<DnsRecordTabBtnViewModel> DnsTabs { get => _dnsTabs; set { _dnsTabs = value; OnPropertyChanged(nameof(DnsTabs)); } }
+
+	private ObservableCollection<DnsItemViewModel> _dnsRecordsItems = [];
+	public ObservableCollection<DnsItemViewModel> DnsRecordsItems { get => _dnsRecordsItems; set { _dnsRecordsItems = value; OnPropertyChanged(nameof(DnsRecordsItems)); } }
 
 	private string _query = string.Empty;
 	public string Query { get => _query; set { _query = value; OnPropertyChanged(nameof(Query)); } }
@@ -59,12 +64,28 @@ public class DnsToolsPageViewModel : ViewModelBase
 
 	public ICommand GetDnsInfoCommand => new RelayCommand(async o =>
 	{
-		await GetDnsInfo();
-		await GetDnsRecords();
+		try
+		{
+			IsRefreshing = true;
+			await GetDnsInfo();
+			await GetDnsRecords();
+			HasInfo = true;
+		}
+		catch (Exception ex)
+		{
+			MessageBox.Show(ex.Message, Properties.Resources.Error, MessageBoxButton.OK, MessageBoxImage.Error);
+			HasInfo = false;
+		}
+		IsRefreshing = false;
 	});
 
 	private bool _hasInfo = false;
-	public bool HasInfo { get => _hasInfo; set { _hasInfo = value; OnPropertyChanged(nameof(HasInfo)); } }
+	public bool HasInfo { get => _hasInfo; set { _hasInfo = value; OnPropertyChanged(nameof(HasInfo)); OnPropertyChanged(nameof(PlaceholderVis)); } }
+
+	private bool _isRefreshing = false;
+	public bool IsRefreshing { get => _isRefreshing; set { _isRefreshing = value; OnPropertyChanged(nameof(IsRefreshing)); OnPropertyChanged(nameof(PlaceholderVis)); } }
+
+	public bool PlaceholderVis => !IsRefreshing && !HasInfo;
 
 	public List<DnsRecord> DnsRecords { get; set; } = [];
 	public DnsToolsPageViewModel()
@@ -85,45 +106,47 @@ public class DnsToolsPageViewModel : ViewModelBase
 
 		IpAddress = ip.ToString();
 		Domain = Query;
+
+		if (response is null) return;
 		CreateDate = response.Registered.ToString() ?? Properties.Resources.Unknown;
 		ExpirationDate = response.Expiration.ToString() ?? Properties.Resources.Unknown;
 		Registrant = response.Registrant.Name ?? Properties.Resources.Unknown;
-		Status = string.Join("", response.DomainStatus) ?? Properties.Resources.Unknown;
-
-		HasInfo = true;
+		Status = string.Join("\n", response.DomainStatus) ?? Properties.Resources.Unknown;
 	}
 
 	private async Task GetDnsRecords()
 	{
 		var lookup = new LookupClient();
 		var result = await lookup.QueryAsync(Query, QueryType.ANY);
-		
+
 		DnsRecords.Clear();
 		DnsTabs.Clear();
-		DnsTabs.Add(new("ANY", this, true));
+		DnsTabs.Add(new("ANY", this, QueryType.ANY, true));
 
 		foreach (var record in result.AllRecords)
 		{
-			DnsRecords.Add(new(record.DomainName, (QueryType)record.RecordType));
+			DnsRecords.Add(new(record.DomainName, record.ToString(), (QueryType)record.RecordType));
 
 			if (DnsTabs.FirstOrDefault(x => x.Title == record.RecordType.ToString()) == null)
-				DnsTabs.Add(new(record.RecordType.ToString(), this));
+				DnsTabs.Add(new(record.RecordType.ToString(), this, (QueryType)record.RecordType));
 		}
+
+		DnsRecordsItems = [.. DnsRecords.Select(x => new DnsItemViewModel(x))];
 	}
 }
 
-public class DnsRecordTabBtnViewModel : ViewModelBase
+public class DnsRecordTabBtnViewModel(string title, DnsToolsPageViewModel dnsToolsPage, QueryType type, bool isChecked = false) : ViewModelBase
 {
-	public string Title { get; set; }
-	public bool IsChecked { get; set; }
-	public ICommand SelectCommand { get; set; }
-	public DnsRecordTabBtnViewModel(string title, DnsToolsPageViewModel dnsToolsPage, bool isChecked = false)
+	public string Title { get; set; } = title;
+	public bool IsChecked { get; set; } = isChecked;
+	public ICommand SelectCommand { get; set; } = new RelayCommand(o =>
 	{
-		Title = title;
-		SelectCommand = new RelayCommand(o =>
+		if (type == QueryType.ANY)
 		{
-			//TODO
-		});
-		IsChecked = isChecked;
-	}
+			dnsToolsPage.DnsRecordsItems = [.. dnsToolsPage.DnsRecords.Select(x => new DnsItemViewModel(x))];
+			return;
+		}
+
+		dnsToolsPage.DnsRecordsItems = [.. dnsToolsPage.DnsRecords.Where(x => x.Type == type).Select(x => new DnsItemViewModel(x))];
+	});
 }
